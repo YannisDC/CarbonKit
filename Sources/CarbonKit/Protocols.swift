81c53d431cb5e09dcb0767b2df7b2034f88d2e9d
Yannis De Cleene <yannisdcl@gmail.com>
2022-02-09 20:16:46 +0100

    Add ozone, methane and nitrous dioxide tracking.
    Added new greenhouse gasses to have better impact estimates.

--- Sources/CarbonKit/Protocols.swift
+++ Sources/CarbonKit/Protocols.swift
@@ -11,6 +11,18 @@ protocol CO2Measurable {
     var CO2: Measurement<UnitMass> { get }
 }
 
+protocol NH4Measurable {
+    var NH4: Measurement<UnitMass> { get }
+}
+
+protocol O3Measurable {
+    var O3: Measurement<UnitMass> { get }
+}
+
+protocol NO2Measurable {
+    var NO2: Measurement<UnitMass> { get }
+}
+
 protocol WaterMeasurable {
     var water: Measurement<UnitVolume> { get }
 }
